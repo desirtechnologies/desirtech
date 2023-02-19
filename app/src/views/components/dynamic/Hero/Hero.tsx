ae22c5e43b0a86a9bf158a224a84ae68e25e60cb
Jeffrey J. Desir <devops@informandinspire.llc>
2023-02-18 19:11:43 -0500

    [... now we can get to it ]
    Tetris Style!

--- app/src/views/components/dynamic/Hero/Hero.tsx
+++ app/src/views/components/dynamic/Hero/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
                         <div className="relative z-20 object-scale-down group">
                             <HackerSVG />
                             <a
-                                className="absolute top-1/2 group-hover:top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 inline-block px-8 py-3.5 text-lg text-center text-gray-200 font-bold font-mono backdrop-blur-sm border-white border-2 border-opacity-25 box-shadow-lg hover:bg-black duration-500 ease-in-out focus:ring-4 focus:ring-gray-200 rounded-full"
+                                className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 inline-block px-8 py-3.5 text-lg text-center text-gray-200 font-bold font-mono backdrop-blur-sm border-white border-2 border-opacity-25 box-shadow-lg hover:bg-black duration-500 ease-in-out focus:ring-4 focus:ring-gray-200 rounded-full"
                                 href="#"
                             >
                                 Technomancer
